ce19f569003714643aa55013893edddba18fa701
vadmurzakov <vadmurzakov@gmail.com>
2019-07-25 23:12:25 +0300

    init repo

---
+++ src/main/java/com/github/vadmurzakov/featuretoggle/FeatureToggleApplication.java
@@ -0,0 +1,13 @@
+package com.github.vadmurzakov.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleApplication.class, args);
+	}
+
+}
