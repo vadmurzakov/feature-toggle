d92a7c1b4583eaf2ec925469a904c79d9e06e5b5
vadmurzakov <vadmurzakov@gmail.com>
2019-07-26 17:43:14 +0300

    Scan exclude object from toString

--- src/main/java/com/github/vadmurzakov/featuretoggle/entity/Scan.java
+++ src/main/java/com/github/vadmurzakov/featuretoggle/entity/Scan.java
@@ -3,14 +3,21 @@ package com.github.vadmurzakov.featuretoggle.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 
 @Data
 @Table(name = "scans")
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(exclude = "object")
 public class Scan {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
