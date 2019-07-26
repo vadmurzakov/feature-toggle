e13681f8eaf6ff4f5ae19eede80cd44d6b2820a1
vadmurzakov <vadmurzakov@gmail.com>
2019-07-26 16:57:13 +0300

    ScanService#convertDangerousFile

--- src/main/java/com/github/vadmurzakov/featuretoggle/service/ScanService.java
+++ src/main/java/com/github/vadmurzakov/featuretoggle/service/ScanService.java
@@ -1,5 +1,6 @@
 package com.github.vadmurzakov.featuretoggle.service;
 
+import com.github.vadmurzakov.featuretoggle.configuration.annotation.Feature;
 import com.github.vadmurzakov.featuretoggle.entity.Scan;
 import com.github.vadmurzakov.featuretoggle.repository.ScanRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
@@ -15,11 +19,22 @@ public class ScanService {
 	private final ScanRepository scanRepository;
 
 	public Scan save(Scan scan) {
-		Scan save = scanRepository.save(scan);
-		return save;
+		convertDangerousFile(scan);
+		return scanRepository.save(scan);
+	}
+
+	public List<Scan> getAll() {
+		return StreamSupport.stream(scanRepository.findAll().spliterator(), false).collect(Collectors.toList());
 	}
 
 	public Scan get(Long id) {
 		return scanRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
+	}
+
+	@Feature(name = "convertDangerousFile")
+	private void convertDangerousFile(Scan scan) {
+		log.info("Scan {} convert to a secure format", scan.getName());
+		scan.setObject("очень безопасный файл".getBytes());
+		scan.setExtension("безопасный формат");
 	}
 }
